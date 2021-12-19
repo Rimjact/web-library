b70282a9a07957fb797bc1f2b68600059da7d96b
Rimjact <70295325+Rimjact@users.noreply.github.com>
2021-12-19 23:58:57 +0200

    Live Search JS added

--- Live Search JS/assets/script.js
+++ Live Search JS/assets/script.js
@@ -4,15 +4,10 @@
 searchInput.addEventListener('input', function() {
     let filter = searchInput.value.toLowerCase();
     
-    if(filter !== '')
-        itemsList.forEach(element => {
-            if(element.textContent.toLowerCase().includes(filter))
+    itemsList.forEach(element => {
+            if(element.textContent.toLowerCase().includes(filter) || filter === '')
                 element.style = 'display: block;';
             else
                 element.style = 'display: none;';
-        });
-    else
-        itemsList.forEach(element => {
-            element.style = 'display: block;';
         });
 })
